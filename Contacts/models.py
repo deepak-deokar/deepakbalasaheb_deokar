13fe0b42e74da40185bd37b68dcd01e88e0ef4e2
deepak-deokar <146700177+deepak-deokar@users.noreply.github.com>
2023-12-10 18:58:39 -0800

    Update models.py

--- Contacts/models.py
+++ Contacts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
+    country = models.CharField(max_length=100, default='United States')
+    phone = PhoneNumberField(null=True, blank=True)
     notes = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
