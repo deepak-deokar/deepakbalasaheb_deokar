d35c94b06127f1f01adaa2869b3a7275533dc7e0
deepak-deokar <146700177+deepak-deokar@users.noreply.github.com>
2023-12-10 18:58:29 -0800

    Update forms.py

--- Contacts/forms.py
+++ Contacts/forms.py
@@ -1,8 +1,14 @@
-# Contacts/forms.py
 from django import forms
 from .models import Contact
+from phonenumber_field.formfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 class ContactForm(forms.ModelForm):
+    phone = PhoneNumberField(widget=PhoneNumberPrefixWidget, required=False)
+
     class Meta:
         model = Contact
-        fields = ['name', 'email', 'notes']
+        fields = ['name', 'email', 'country', 'phone', 'notes']
+        widgets = {
+            'country': forms.TextInput(attrs={'placeholder': 'Country'}),
+        }
