c70b16d131caf45785477cae3fd0a9a3e3da09f8
deepak-deokar <146700177+deepak-deokar@users.noreply.github.com>
2023-12-10 18:58:52 -0800

    Update views.py

--- Contacts/views.py
+++ Contacts/views.py
@@ -1,7 +1,8 @@
 
 from django.urls import reverse_lazy
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.contrib import messages
 from .models import Contact
 from .forms import ContactForm
 
@@ -15,10 +16,6 @@ class ContactCreateView(CreateView):
     form_class = ContactForm
     success_url = reverse_lazy('Contacts:contact_list')
 
-
-    #def get_success_url(self):
-     #   return reverse_lazy('Contacts:contact_list')
-
 class ContactUpdateView(UpdateView):
     model = Contact
     template_name = 'Contact_form.html'
@@ -30,6 +27,15 @@ class ContactDeleteView(DeleteView):
     template_name = 'Contact_confirm_delete.html'
     success_url = reverse_lazy('Contacts:contact_list')
 
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        contact_name = self.object.name  
+        response = super(ContactDeleteView, self).delete(request, *args, **kwargs)
+        success_message = f'"{self.object.name}" has been successfully deleted.'
+        messages.success(request, success_message)
+        return response
+
 class ContactDetailView(DetailView):
     model = Contact
     template_name = 'Contact_detail.html'
+
