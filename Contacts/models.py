6a306109edd9ba42e2ca86b574cb804727c1cf6a
deepak-deokar <146700177+deepak-deokar@users.noreply.github.com>
2023-12-11 18:25:07 -0800

    fields updated

--- Contacts/models.py
+++ Contacts/models.py
@@ -5,8 +5,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Contact(models.Model):
     name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
-    country = models.CharField(max_length=100, default='United States')
-    phone = PhoneNumberField(null=True, blank=True)
+    country = models.CharField(max_length=100, default='country')
+    phone = PhoneNumberField(max_length=10, default='1234567890')
     notes = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
