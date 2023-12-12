bbe2e2902aa43e35c1fdece8a38c35a41dc81e44
deepak-deokar <146700177+deepak-deokar@users.noreply.github.com>
2023-12-11 18:33:57 -0800

    updating models.py

--- Contacts/models.py
+++ Contacts/models.py
@@ -6,7 +6,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
     country = models.CharField(max_length=100, default='country')
-    phone = PhoneNumberField(max_length=10, default='1234567890')
+    phone = PhoneNumberField(max_length=13, default='1234567890')
     notes = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
